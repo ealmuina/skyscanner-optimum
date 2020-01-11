e4e1dfff6879057e4dcc6a9038f0af59402a9f0d
Eddy Almuiña <almuina.e@gmail.com>
2020-01-11 13:04:28 +0100

    Modified running script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3 &
-python3 bot.py 2> log.txt
+(
+  echo sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3;
+  echo python3 bot.py 2> $HOME/skyscanner-optimum/log.txt
+) | parallel
