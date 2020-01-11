7e45e1c1cc1b20b084937d94b4dfae9670d7ffeb
Eddy Almuiña <almuina.e@gmail.com>
2020-01-11 14:08:11 +0100

    Bugfix: redirecting both stdout and stderr attempt 2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd "$HOME/skyscanner-optimum/" || exit
 (
   echo "sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3";
-  echo "python3 bot.py > log.txt 2> &1"
+  echo "python3 bot.py &> log.txt"
 ) | parallel
