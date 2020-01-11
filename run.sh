4aee781b23ecb7e4c99c55687872fa448e0cb0c6
Eddy Almuiña <almuina.e@gmail.com>
2020-01-11 13:10:17 +0100

    Bugfix: echo for parallel

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 (
-  echo sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3;
-  echo python3 bot.py 2> $HOME/skyscanner-optimum/log.txt
+  echo "sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3";
+  echo "python3 bot.py 2> $HOME/skyscanner-optimum/log.txt"
 ) | parallel
