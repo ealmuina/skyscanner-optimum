8f8781e752d76fcb792bce49a0499881f0bc4045
Eddy Almuiña <almuina.e@gmail.com>
2020-01-11 13:18:53 +0100

    Bugfix: database path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 (
-  echo "sqlite_web -H 0.0.0.0 -p 7070 db.sqlite3";
+  echo "sqlite_web -H 0.0.0.0 -p 7070 $HOME/skyscanner-optimum/db.sqlite3";
   echo "python3 bot.py 2> $HOME/skyscanner-optimum/log.txt"
 ) | parallel
